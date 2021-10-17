a2f2a1777912cec0f16853af4e27833e6c46226c
tongchangD <15527098078@163.com>
2021-10-18 00:37:51 +0800

    add source code

---
+++ config.py
@@ -0,0 +1,17 @@
+# word frequence file
+word_freq_path = './data/word_freq.txt'
+# char set file
+common_char_path = './data/common_char_set.txt'
+# same pinyin char file
+same_pinyin_path = './data/same_pinyin.txt'
+# custom confusion set
+custom_confusion_path = './data/custom_confusion.txt'
+# custom word for segment
+custom_word_path = './data/custom_word.txt'
+
+similar_pinyin_path='./data/custom_similar.txt'
+
+# language model path
+language_model_path = './data/kenlm/people_chars_lm.klm'
+
+excel_path = "./dataset/v2.2改正版本_11_07.xlsx"
